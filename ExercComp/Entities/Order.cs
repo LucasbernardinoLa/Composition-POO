6332a7393c8c37a78cb62edb58b8af0790a85b64
Lucas Bernardino <lucasblameu@gmail.com>
2021-12-28 20:02:58 -0300

    Exercício de fixação de composition

---
+++ ExercComp/Entities/Order.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+using ExercComposition.Entities.Enums;
+
+namespace ExercComposition.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItems(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItems(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem items in Items)
+            {
+                sum += items.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order sumary");
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.AppendLine($"{Client.Name} {Client.BirthDate.ToString("(dd/MM/yyyy)")} {Client.Email}");
+            sb.AppendLine("Order items: ");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine($"{item.Product.Name}, ${item.Price}, Quantity: {item.Quantity}, ${item.SubTotal()}");
+            }
+            sb.Append($"Total price: ");
+            sb.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+
+
+        }
+    }
+}
---
+++ ExercComp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using ExercComposition.Entities;
+using ExercComposition.Entities.Enums;
+
+namespace ExercComposition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, date);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data");
+                Console.Write("Product name: ");
+                string prodName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double prodPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Product Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                Product product = new Product(prodName, prodPrice);
+                OrderItem orderItem = new OrderItem(quantity, prodPrice, product);
+                order.AddItems(orderItem);
+            }
+            Console.WriteLine(order);
+        }
+    }
+}
